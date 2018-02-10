80c7f587537ba1858129d1d44704a8233e44fcd0
ranjinidas <das22r@mtholyoke.edu>
2018-02-10 17:35:37 -0500

    Tried qsort

--- TextSort.c
+++ TextSort.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 void getFileContent(const char *name, char *arr[][100]);
+//int compareStrings(const void **str1, const void **str2);
+void sortFile(char *arr[10][100]);
 int main(int argc, char *argv[]){
+	printf("in main");
 	char stringArr[10][100];
 	char *fileName= &(*argv[1]);
 	printf("%s", fileName);	
 	getFileContent(fileName, &stringArr);
 	printf("at index 1");
 //	printf("%s", stringArr[1][100]);
+
+	char str1[100] = "Hello"; 
+	char str2[100] = "World";
+//	compareStrings(&str1, &str2); 
 }
 
 void getFileContent(const char *name, char *arr[10][100]){
@@ -28,3 +36,25 @@ void getFileContent(const char *name, char *arr[10][100]){
 	}
 	fclose(fp);	
 }
+
+int compareStrings (const void *elem1, const void *elem2) {
+	printf("%s", "Here");
+	/**cast to actual type**/
+	char **strptr1 = malloc(sizeof(char **));
+	strptr1 = (char **) elem1; 
+	char **strptr2 = malloc(sizeof(char **));
+	**strptr2 = (char **) elem2; 
+	/**dereference to get strings**/	
+	char *str1 = *strptr1; 
+	char *str2 = *strptr2;	
+	int diff;	
+	diff = strcmp (str1, str2); 
+	printf("%d", diff); 
+	return diff; 
+}
+	
+
+void sortFile(char *arr[10][100]) {
+	qsort(*arr, 10, 100, compareStrings); 
+}
+
